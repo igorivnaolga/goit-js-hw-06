44abfa230fdad11dbb2eb42750bab5ca48b448e6
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-08-25 17:47:27 +1200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngridients = document.querySelector('#ingredients');
+
+const markup = ingredients.map((ingridient) => {
+  const li = document.createElement('li');
+  li.textContent = ingridient;
+  li.classList = 'ingridient';
+  console.log(li);
+  return li;
+});
+
+listOfIngridients.append(...markup);
