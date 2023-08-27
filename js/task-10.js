a4771dfb8f4befe46df25857b6087467573fe195
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-08-27 19:53:54 +1200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,27 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const input = document.querySelector('input');
+// const create = document.querySelector('[data-create]');
+// const destroy = document.querySelector('[data-destroy]');
+// const boxes = document.querySelector('#boxes');
+// const amount = input.value;
+// function createBoxes(amount) {
+//   for (i = 1; i <= amount; i++) {
+//     let box = ('<div></div>'.style.width = '10');
+//     height = '10';
+//     color = 'getRandomHexColor()';
+//   }
+// }
+
+// const handleCreate = (event) => {
+//   createBoxes();
+// };
+
+// const handleDestroy = (event) => {
+//   boxes.innerHTML = '';
+// };
+
+// create.addEventListener('click', handleCreate);
+// destroy.addEventListener('click', handleDestroy);
